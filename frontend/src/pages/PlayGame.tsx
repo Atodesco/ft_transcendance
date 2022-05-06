5def87215599fd3affebe73454e7e9ed8b1c5a61
lacruype <lacruype@student.42.com>
2022-05-07 00:11:33 +0200

    Boutton de Queue qui existe

--- frontend/src/pages/PlayGame.tsx
+++ frontend/src/pages/PlayGame.tsx
@@ -1,5 +1,47 @@
 import styles from "../css/PlayGame.module.css";
+import Button from "../components/Button";
+import React, { Component, useState } from "react";
+import { useStopwatch } from "react-timer-hook";
+import { getElementError } from "@testing-library/react";
+import { blob } from "stream/consumers";
 
 export default function PlayGame() {
-	return <div>CECI EST LA PAGE DE JEU</div>;
+  const { seconds, minutes, hours, days, isRunning, start, pause, reset } =
+    useStopwatch({ autoStart: false });
+  const [booleanButton, setBooleanButton] = useState(false);
+  const [textQueueButton, setTextQueueButton] = useState("Queue Up");
+  return (
+    <div className={styles.game}>
+      <div className={styles.Buttons}>
+        <Button
+          id={styles.queueUpButton}
+          onClick={() => {
+            if (!booleanButton) {
+              start();
+              const queueTimer = document.getElementById(styles.timer);
+              if (queueTimer) {
+                queueTimer.style.opacity = "1";
+              }
+              setTextQueueButton("Leave Queue");
+            } else {
+              reset();
+              const queueTimer = document.getElementById(styles.timer);
+              if (queueTimer) {
+                queueTimer.style.opacity = "0";
+              }
+              setTextQueueButton("Queue Up");
+            }
+            setBooleanButton(!booleanButton);
+          }}
+          text={textQueueButton}
+        />
+        <Button text="Spectate" />
+      </div>
+      <div>
+        <div id={styles.timer}>
+          <span>{minutes}</span> : <span>{seconds}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
