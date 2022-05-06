1824e559b910637b87df1a30c16b685aba185b0c
rledrin <remi.ledrin@protonmail.com>
2022-05-06 16:37:49 +0200

    Leaderboard + friendlist front

---
+++ frontend/src/pages/Friendlist/List.tsx
@@ -0,0 +1,24 @@
+import Profiles from "../Leaderboard/profile";
+// import data from "./ListData.json";
+import { Data } from "../Leaderboard/database";
+
+interface Props {
+	input: string;
+}
+
+function List(props: Props) {
+	//create a new array by filtering the original array
+	const filteredData = Data.filter((el) => {
+		//if no input the return the original
+		if (props.input === "") {
+			return el;
+		}
+		//return the item which contains the user input
+		else {
+			return el.name.toLowerCase().includes(props.input);
+		}
+	});
+	return <Profiles Leaderboard={filteredData} />;
+}
+
+export default List;
