4482774c5450f6defeed861e08f63f5a3e7ebc23
lacruype <lacruype@student.42.com>
2022-05-09 14:56:12 +0200

    machin

--- frontend/src/pages/Credits.tsx
+++ frontend/src/pages/Credits.tsx
@@ -1,7 +1,21 @@
-import styles from "../css/Credits.module.css"
+import Button from "../components/Button";
+import styles from "../css/Credits.module.css";
 
 export default function Credits() {
   return (
-	<div>Credits</div>
-  )
+    <div>
+      Credits
+      <button
+        onClick={async () => {
+          const username = await fetch("http://localhost:3000/user/", {
+            credentials: "include",
+          });
+          const data = await username.json();
+          console.log(data);
+        }}
+      >
+        CLICK
+      </button>
+    </div>
+  );
 }
