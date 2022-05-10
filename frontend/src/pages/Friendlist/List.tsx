5a5faeee3a1933a293b9e9c846070344b3e83ea4
atodesco <todesco.alexandre34@gmail.com>
2022-05-10 16:07:08 +0200

    Co-authored-by: rledrin <rledrin@users.noreply.github.com> Co-authored-by: lacruype <lacruype@users.noreply.github.com>

--- frontend/src/pages/Friendlist/List.tsx
+++ frontend/src/pages/Friendlist/List.tsx
@@ -1,23 +1,61 @@
-import Profiles from "../Leaderboard/profile";
+// import Profiles from "../Leaderboard/profile";
+import Profiles from "./Users";
 import { Data } from "../Leaderboard/database";
+import { setEnvironmentData } from "worker_threads";
+import { useEffect, useState } from "react";
 
 interface Props {
-	input: string;
+  input: string;
 }
 
 function List(props: Props) {
-	//create a new array by filtering the original array
-	const filteredData = Data.filter((el) => {
-		//if no input the return the original
-		if (props.input === "") {
-			return el;
-		}
-		//return the item which contains the user input
-		else {
-			return el.name.toLowerCase().includes(props.input);
-		}
-	});
-	return <Profiles Leaderboard={filteredData} />;
+  const aVar: any = [{}];
+
+  let filteredData: any = [{ username: "", picture: "", status: "" }];
+  const [finalData, setFinalData] = useState(filteredData);
+  const getData = async () => {
+    const rawData = await fetch(
+      process.env.REACT_APP_BACK_URL +
+        ":" +
+        process.env.REACT_APP_BACK_PORT +
+        "/user/",
+      {
+        credentials: "include",
+      }
+    );
+    const alldata = await rawData.json();
+    const rawData2 = await fetch(
+      process.env.REACT_APP_BACK_URL +
+        ":" +
+        process.env.REACT_APP_BACK_PORT +
+        "/user/me",
+      {
+        credentials: "include",
+      }
+    );
+    const myData = await rawData2.json();
+
+    const filteredData = alldata.filter((el: any) => {
+      if (
+        el.ft_id !== myData.ft_id &&
+        myData.friends &&
+        myData.friends.includes(el.ft_id)
+      ) {
+        if (props.input === "") {
+          return el;
+        } else {
+          return el.username.toLowerCase().includes(props.input);
+        }
+      }
+    });
+    setFinalData(filteredData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <Profiles Data={finalData} />;
 }
 
 export default List;
