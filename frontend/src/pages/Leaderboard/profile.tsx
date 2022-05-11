1d6651884ddd03448b9f8f8d28317c0e71cdae89
atodesco <todesco.alexandre34@gmail.com>
2022-05-11 16:45:35 +0200

    leaderboard css

--- frontend/src/pages/Leaderboard/profile.tsx
+++ frontend/src/pages/Leaderboard/profile.tsx
@@ -1,4 +1,5 @@
 import styles from "../../css/Leaderboard.module.css";
+import { Link } from "react-router-dom";
 
 export default function profiles({ Leaderboard }: any) {
   return <div id={styles.profile}>{Item(Leaderboard)}</div>;
@@ -7,29 +8,37 @@ export default function profiles({ Leaderboard }: any) {
 function Item(data: any[]) {
   return (
     <>
-      {data.map((value, index) => (
-        <div className={styles.flex} key={index}>
-          <div />
-          <div className={styles.item}>
-            <img src={value.img} alt="" />
+      {data.map((value, index) => {
+        let color;
+        if (value.status == "Online") color = "green";
+        else if (value.status == "Offline") color = "red";
+        else if (value.status == "InGame") color = "orange";
+        return (
+          <div className={styles.flex} key={index}>
+            <div className={styles.item}>
+              <img src={value.picture} alt="" />
 
-            <div className={styles.info}>
-              <h3 className={`${styles.name} ${styles.textDark}`}>
-                {value.name}
-              </h3>
-              <span>{value.status}</span>
+              <div className={styles.info}>
+                <Link to={"/Profile/" + value.ft_id}>
+                  <h3 className={`${styles.name} ${styles.textDark}`}>
+                    {value.username}
+                  </h3>
+                </Link>
+              </div>
+              <div className={styles.status} style={{ color: color }}>
+                <span>{value.status}</span>
+              </div>
+
+              <div className={styles.elo}>
+                <span>Elo: {value.elo}</span>
+              </div>
+              <div className={styles.rank}>
+                <span>Rank: {index + 1}</span>
+              </div>
             </div>
           </div>
-          <div />
-          <div />
-          <div />
-          <div />
-          <div className={styles.item}>
-            <span>{value.elo}</span>
-          </div>
-          <div />
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 }
