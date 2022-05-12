191c64a3b81e7c32138deeef72113915efd56309
rledrin <remi.ledrin@protonmail.com>
2022-05-12 18:47:46 +0200

    Co-authored-by: lacruype <lacruype@users.noreply.github.com>

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -1,10 +1,10 @@
 import {
-  SubscribeMessage,
-  WebSocketGateway,
-  OnGatewayInit,
-  WebSocketServer,
-  OnGatewayConnection,
-  OnGatewayDisconnect,
+	SubscribeMessage,
+	WebSocketGateway,
+	OnGatewayInit,
+	WebSocketServer,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
 } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -14,144 +14,152 @@ import { Repository } from "typeorm";
 const bcrypt = require("bcrypt");
 
 @WebSocketGateway({
-  cors: {
-    origin: "*",
-  },
+	cors: {
+		origin: "*",
+	},
 })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  constructor(
-    @InjectRepository(Channel)
-    private readonly channelRepository: Repository<Channel>,
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>
-  ) {}
+	constructor(
+		@InjectRepository(Channel)
+		private readonly channelRepository: Repository<Channel>,
+		@InjectRepository(User)
+		private readonly userRepository: Repository<User>
+	) {}
 
-  @WebSocketServer()
-  server: Server;
+	@WebSocketServer()
+	server: Server;
 
-  private clientsId = new Map<string, number>();
-  private clientsFt = new Map<number, string>();
+	private clientsId = new Map<string, number>();
+	private clientsFt = new Map<number, string>();
 
-  handleConnection(client: Socket) {
-    console.log("Client connected: ", client.id);
-    console.log("Client ft_id: ", client.handshake.query.ft_id);
-    this.clientsId.set(client.id, Number(client.handshake.query.ft_id));
-    this.clientsFt.set(Number(client.handshake.query.ft_id), client.id);
-  }
+	handleConnection(client: Socket) {
+		console.log("Client connected: ", client.id);
+		console.log("Client ft_id: ", client.handshake.query.ft_id);
+		this.clientsId.set(client.id, Number(client.handshake.query.ft_id));
+		this.clientsFt.set(Number(client.handshake.query.ft_id), client.id);
+	}
 
-  handleDisconnect(client: Socket) {
-    console.log("Client disconnected: ", client.id);
-    const ft_id = this.clientsId.get(client.id);
-    this.clientsId.delete(client.id);
-    this.clientsFt.delete(ft_id);
-  }
+	handleDisconnect(client: Socket) {
+		console.log("Client disconnected: ", client.id);
+		const ft_id = this.clientsId.get(client.id);
+		this.clientsId.delete(client.id);
+		this.clientsFt.delete(ft_id);
+	}
 
-  emitChannel(channel: any, event: string, data: any): void {
-    try {
-      if (!channel.users) return;
+	emitChannel(channel: any, event: string, data: any): void {
+		try {
+			if (!channel.users) return;
 
-      const sockets: any[] = Array.from(this.server.sockets.sockets.values());
-      sockets.forEach((socket) => {
-        if (
-          channel.users.find(
-            (user) => user.ft_id === this.clientsId.get(socket.data.user.id)
-          )
-        ) {
-          socket.emit(event, data);
-        }
-      });
-    } catch {}
-  }
+			const sockets: any[] = Array.from(this.server.sockets.sockets.values());
+			sockets.forEach((socket) => {
+				if (
+					channel.users.find(
+						(user) => user.ft_id === this.clientsId.get(socket.id)
+					)
+				) {
+					socket.emit(event, data);
+				}
+			});
+		} catch {}
+	}
 
-  @SubscribeMessage("text")
-  async handleMessage(
-    client: Socket,
-    data: { channelname: string; message: string; date: Date }
-  ): Promise<void> {
-    console.log("Message received from: ", client.id);
-    console.log("Message received data: ", data);
-    const ft_id = this.clientsId.get(client.id);
+	@SubscribeMessage("text")
+	async handleMessage(
+		client: Socket,
+		data: { channelname: string; message: string; date: Date }
+	): Promise<void> {
+		console.log("Message received from: ", client.id);
+		console.log("Message received data: ", data);
+		const ft_id = this.clientsId.get(client.id);
 
-    const channel = await this.channelRepository.findOne({
-      channelname: data.channelname,
-    });
-    if (channel && channel.users) {
-      const user = channel.users.find((user) => user.ft_id === ft_id);
-      if (user) {
-        this.emitChannel(channel, "text", {
-          message: data.message,
-          user: user,
-          date: data.date,
-        });
-      }
-    }
-  }
+		const channel = await this.channelRepository.findOne({
+			channelname: data.channelname,
+		});
+		if (channel && channel.users) {
+			const user = channel.users.find((user) => user.ft_id === ft_id);
+			if (user) {
+				// this.emitChannel(channel, "text", {
+				this.server.emit("text", {
+					message: data.message,
+					user: user,
+					date: data.date,
+				});
+			}
+		}
+	}
 
-  @SubscribeMessage("joinChannel")
-  async joinChannel(
-    client: Socket,
-    data: { channelId: number }
-  ): Promise<void> {
-    const ft_id = this.clientsId.get(client.id);
-    const user = await this.userRepository.findOne({ ft_id: ft_id });
-    const channel = await this.channelRepository.findOne({
-      id: data.channelId,
-    });
-    if (!user.channels) {
-      user.channels = [channel.id];
-    } else {
-      user.channels.push(channel.id);
-    }
-    if (!channel.users) {
-      channel.users = [user];
-    } else {
-      channel.users.push(user);
-    }
-    await this.userRepository.save(user);
-    await this.channelRepository.save(channel);
-  }
+	@SubscribeMessage("joinChannel")
+	async joinChannel(
+		client: Socket,
+		data: { channelId: number }
+	): Promise<void> {
+		const ft_id = this.clientsId.get(client.id);
+		const user = await this.userRepository.findOne({ ft_id: ft_id });
+		const channel = await this.channelRepository.findOne({
+			id: data.channelId,
+		});
+		if (!user.channels) {
+			user.channels = [channel.id];
+		} else {
+			user.channels.push(channel.id);
+		}
+		if (!channel.users) {
+			channel.users = [user];
+		} else {
+			channel.users.push(user);
+		}
+		await this.userRepository.save(user);
+		await this.channelRepository.save(channel);
+		client.emit(
+			"myChannel",
+			await this.channelRepository.findOne({ id: channel.id })
+		);
+	}
 
-  @SubscribeMessage("createChannel")
-  async createChannel(
-    client: Socket,
-    data: { channelname: string; password: string }
-  ): Promise<void> {
-    const ft_id = this.clientsId.get(client.id);
-    const user = await this.userRepository.findOne({ ft_id: ft_id });
-    let channel = this.channelRepository.create();
-    channel.channelname = data.channelname;
-    channel.owner = user;
-    channel.private = data.password ? true : false;
-    const saltRounds = 10;
-    if (data.password) {
-      bcrypt.genSalt(saltRounds, function (err, salt) {
-        bcrypt.hash(data.password, salt, function (err, hash) {
-          // ! Store le hash dans la base de données
-          hash;
-        });
-      });
-    }
+	@SubscribeMessage("createChannel")
+	async createChannel(
+		client: Socket,
+		data: { channelname: string; password: string }
+	): Promise<void> {
+		const ft_id = this.clientsId.get(client.id);
+		const user = await this.userRepository.findOne({ ft_id: ft_id });
+		let channel = this.channelRepository.create();
+		channel.channelname = data.channelname;
+		channel.owner = user;
+		channel.private = data.password ? true : false;
+		const saltRounds = 10;
+		if (data.password) {
+			bcrypt.genSalt(saltRounds, function (err, salt) {
+				bcrypt.hash(data.password, salt, function (err, hash) {
+					// ! Store le hash dans la base de données
+					hash;
+				});
+			});
+		}
 
-    if (!channel.users) {
-      channel.users = [user];
-    } else {
-      channel.users.push(user);
-    }
-    await this.channelRepository.save(channel);
+		if (!channel.users) {
+			channel.users = [user];
+		} else {
+			channel.users.push(user);
+		}
+		await this.channelRepository.save(channel);
 
-    if (!user.channels) {
-      user.channels = [channel.id];
-    } else {
-      user.channels.push(channel.id);
-    }
-    await this.userRepository.save(user);
-    const sockets: any[] = Array.from(this.server.sockets.sockets.values());
+		if (!user.channels) {
+			user.channels = [channel.id];
+		} else {
+			user.channels.push(channel.id);
+		}
+		await this.userRepository.save(user);
+		const sockets: any[] = Array.from(this.server.sockets.sockets.values());
 
-
-    sockets.forEach((socket) => {
-      if (socket.id !== client.id) {
-        socket.emit("searchChannel", channel);
-      }
-    });
-  }
+		sockets.forEach((socket) => {
+			if (socket.id !== client.id) {
+				socket.emit("searchChannel", channel);
+			}
+		});
+		client.emit(
+			"myChannel",
+			await this.channelRepository.findOne({ id: channel.id })
+		);
+	}
 }
--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -18,241 +18,250 @@ import { io } from "socket.io-client";
 import { ftruncate } from "fs";
 
 export default function Chat() {
-  const [inputText, setInputText] = useState("");
-  const [messages, setMessages] = useState("");
-  const [openModal, setOpenModal] = React.useState(false);
-  const [password, setPassword] = useState("");
-  const [channelName, setChannelName] = useState("");
-  const [databaseChannel, setDatabaseChannel] = useState<any>();
-  const [channelUserJoined, setChannelUserJoined] = useState<any>();
-  const [channelSelected, setChannelSelected] = useState(0);
+	const [inputText, setInputText] = useState("");
+	const [messages, setMessages] = useState("");
+	const [openModal, setOpenModal] = React.useState(false);
+	const [password, setPassword] = useState("");
+	const [channelName, setChannelName] = useState("");
+	const [databaseChannel, setDatabaseChannel] = useState<any>([]);
+	const [channelUserJoined, setChannelUserJoined] = useState<any>([]);
+	const [channelSelected, setChannelSelected] = useState(0);
+	const [searchBarState, setSearchBarState] = useState<any>();
 
-  const p: any = { ini: 1 };
-  let [userInfo, setUserInfo] = useState<any>(p);
-  let ws = useRef(p);
-  let flag = useRef(p);
+	const p: any = { ini: 1 };
+	let [userInfo, setUserInfo] = useState<any>(p);
+	let ws = useRef(p);
+	let flag = useRef(p);
 
-  const getUserInfo = async () => {
-    const myData = await fetch(
-      process.env.REACT_APP_BACK_URL +
-        ":" +
-        process.env.REACT_APP_BACK_PORT +
-        "/user/me/",
-      {
-        credentials: "include", //this is what I need to tell the browser to include cookies
-      }
-    );
-    setUserInfo(await myData.json());
-  };
+	const getUserInfo = async () => {
+		const myData = await fetch(
+			process.env.REACT_APP_BACK_URL +
+				":" +
+				process.env.REACT_APP_BACK_PORT +
+				"/user/me/",
+			{
+				credentials: "include", //this is what I need to tell the browser to include cookies
+			}
+		);
+		setUserInfo(await myData.json());
+	};
 
-  useEffect(() => {
-    if (flag.current.ini) {
-      getUserInfo();
-      flag.current = {};
-    }
-    getChannels();
-  }, []);
+	useEffect(() => {
+		if (flag.current.ini) {
+			getUserInfo();
+			flag.current = {};
+		}
+		getChannels();
+	}, []);
 
-  useEffect(() => {
-    if (ws.current.ini && userInfo.ft_id) {
-      ws.current = io("http://localhost:3000?ft_id=" + userInfo.ft_id);
-      ws.current.on("text", (data: any) => {
-        setMessages((messages: any) => {
-          let newMessage = JSON.parse(messages);
-          newMessage.push({
-            message: data,
-            user: userInfo.ft_id,
-            date: "2020-01-01",
-          });
-          return JSON.stringify(newMessage);
-        });
-      });
-      ws.current.on("searchChannel", (channel: any) => {
-        setDatabaseChannel((c: any) => {
-          c.push(channel);
-          return c;
-        });
-      });
-      ws.current.on("myChannel", (channel: any) => {
-        setChannelUserJoined((c: any) => {
-          c.push(channel);
-          return c;
-        });
-      });
-    }
-  }, [userInfo]);
+	useEffect(() => {
+		if (ws.current.ini && userInfo.ft_id) {
+			ws.current = io("http://localhost:3000?ft_id=" + userInfo.ft_id);
+		}
+	}, [userInfo]);
 
-  useEffect(() => {
-    if (userInfo.ft_id) {
-      let arr: any = [];
-      databaseChannel.map((value: any, index: any) => {
-        if (userInfo.channels.includes(value.id)) {
-          arr.push(value);
-        }
-      });
+	useEffect(() => {
+		if (userInfo.ft_id) {
+			let arr: any = [];
+			databaseChannel.map((value: any, index: any) => {
+				if (userInfo.channels.includes(value.id)) {
+					arr.push(value);
+				}
+			});
+			setChannelUserJoined(arr);
 
-      setChannelUserJoined(arr);
-    }
-  }, [databaseChannel]);
+			ws.current.on("searchChannel", (channel: any) => {
+				let tmp = databaseChannel.slice();
+				tmp.push(channel);
+				setDatabaseChannel(tmp);
+			});
+			setSearchBarState(
+				databaseChannel.filter((channel: any) => {
+					if (!userInfo.channels.includes(channel.id)) {
+						return channel.channelname;
+					}
+				})
+			);
+		}
+	}, [databaseChannel]);
 
-  useEffect(() => {
-    console.log("OUI", channelUserJoined);
-  }, [channelUserJoined]);
+	useEffect(() => {
+		if (userInfo.ft_id) {
+			ws.current.on("myChannel", (channel: any) => {
+				let tmp = channelUserJoined.slice();
+				tmp.push(channel);
+				setChannelUserJoined(tmp);
+			});
+		}
+	}, [channelUserJoined]);
 
-  let getChannels = async () => {
-    const rawData = await fetch(
-      process.env.REACT_APP_BACK_URL +
-        ":" +
-        process.env.REACT_APP_BACK_PORT +
-        "/chat/channel",
-      { credentials: "include" }
-    );
-    const data = await rawData.json();
-    setDatabaseChannel(data);
-  };
-  let selectChannel = () => {
-    Chatabase.map((value: any, index: any) => {
-      if (value.channelname === channelSelected)
-        setMessages(JSON.stringify(value.messages));
-    });
-  };
-  useEffect(() => {
-    selectChannel();
-  }, [channelSelected]);
-  const handleOpenModal = () => {
-    setOpenModal(true);
-  };
-  const handleCloseModal = () => {
-    setOpenModal(false);
-  };
-  return (
-    <div>
-      <div className={styles.searchChannels}>
-        <div className={styles.search}>
-          <Autocomplete
-            disablePortal
-            id="combo-box-demo"
-            options={databaseChannel}
-            sx={{ width: 673 }}
-            getOptionLabel={(channel: any) => {
-              if (!userInfo.channels.includes(channel.id)) {
-                return channel.channelname;
-              }
-            }}
-            onChange={async (event, value) => {
-              if (value) {
-                const myData = await fetch(
-                  process.env.REACT_APP_BACK_URL +
-                    ":" +
-                    process.env.REACT_APP_BACK_PORT +
-                    "/user/me/",
-                  {
-                    credentials: "include", //this is what I need to tell the browser to include cookies
-                  }
-                );
-                const data = await myData.json();
-                const TOTALCHANNEL = await fetch(
-                  process.env.REACT_APP_BACK_URL +
-                    ":" +
-                    process.env.REACT_APP_BACK_PORT +
-                    "/user/" +
-                    data.ft_id +
-                    "/joinChannel/" +
-                    value.id,
-                  { credentials: "include" }
-                );
-                console.log("SALUTCESTMOI");
-              }
-            }}
-            renderInput={(params) => (
-              <TextField {...params} label="Search Channel" />
-            )}
-          />
-        </div>
-      </div>
-      <div className={styles.chat}>
-        <div className={styles.chatHistory}>
-          {messages && <Messages myMessages={JSON.parse(messages)}></Messages>}
-        </div>
-        <div className={styles.chatBar}>
-          <TextField
-            variant="outlined"
-            fullWidth
-            label={channelSelected ? "Enter a message ..." : "Select a channel"}
-            onChange={(e: any) => setInputText(e.target.value)}
-            value={inputText}
-            disabled={!channelSelected}
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && inputText) {
-                const channel = databaseChannel.find((value: any) => {
-                  return value.id === channelSelected;
-                });
-                const dataToSend = {
-                  channelname: channel?.channelname,
-                  message: inputText,
-                };
-                ws.current.emit("text", dataToSend);
-                setInputText("");
-              }
-            }}
-          />
-        </div>
-      </div>
-      <div className={styles.myChats}>
-        <h1>My Chats</h1>
-        <div className={styles.button}>
-          <Button text="Create Channel" onClick={handleOpenModal}>
-            <FontAwesomeIcon icon={faPlusCircle} />
-          </Button>
-          <Modal
-            open={openModal}
-            onClose={handleCloseModal}
-            className={styles.modal}
-          >
-            <Box
-              component="form"
-              sx={{
-                "& > :not(style)": { m: 1, width: "25ch" },
-              }}
-              autoComplete="off"
-              className={styles.boxModal}
-            >
-              <TextField
-                label="Enter a Name ..."
-                variant="standard"
-                onChange={(e: any) => setChannelName(e.target.value)}
-                value={channelName}
-              />
-              <TextField
-                label="Choose a Password ..."
-                variant="standard"
-                type="password"
-                onChange={(e: any) => setPassword(e.target.value)}
-                value={password}
-              />
-              <Button
-                text="Create"
-                className={styles.create}
-                onClick={async () => {
-                  handleCloseModal();
-                  ws.current.emit("createChannel", {
-                    channelname: channelName,
-                    password: password,
-                  });
-                  setPassword("");
-                  setChannelName("");
-                }}
-              />
-            </Box>
-          </Modal>
-        </div>
-        <div className={styles.trucblanc}>
-          <Channels
-            myChats={channelUserJoined}
-            channelState={setChannelSelected}
-          ></Channels>
-        </div>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		if (userInfo.ft_id) {
+			ws.current.on("text", (data: any) => {
+				console.log("SALUT :", data);
+				let newTmp = JSON.parse(messages.slice());
+				newTmp.push({
+					message: data,
+					user: userInfo.ft_id,
+					date: "2020-01-01",
+				});
+				setMessages(JSON.stringify(newTmp));
+			});
+		}
+	}, [messages]);
+
+	let getChannels = async () => {
+		const rawData = await fetch(
+			process.env.REACT_APP_BACK_URL +
+				":" +
+				process.env.REACT_APP_BACK_PORT +
+				"/chat/channel",
+			{ credentials: "include" }
+		);
+		const data = await rawData.json();
+		setDatabaseChannel(data);
+	};
+	let selectChannel = () => {
+		Chatabase.map((value: any, index: any) => {
+			if (value.channelname === channelSelected)
+				setMessages(JSON.stringify(value.messages));
+		});
+	};
+	useEffect(() => {
+		selectChannel();
+	}, [channelSelected]);
+	const handleOpenModal = () => {
+		setOpenModal(true);
+	};
+	const handleCloseModal = () => {
+		setPassword("");
+		setChannelName("");
+		setOpenModal(false);
+	};
+	return (
+		<div>
+			<div className={styles.searchChannels}>
+				<div className={styles.search}>
+					<Autocomplete
+						disablePortal
+						clearOnEscape={true}
+						options={searchBarState}
+						sx={{ width: 673 }}
+						// filterOptions={(options: any, state: object) => {
+						// 	return options.filter((channel: any) => {
+						// 		if (!userInfo.channels.includes(channel.id)) {
+						// 			return channel.channelname;
+						// 		}
+						// 	});
+						// }}
+						getOptionLabel={(channel: any) => {
+							return channel.channelname;
+						}}
+						value={searchBarState}
+						onChange={async (event, value) => {
+							if (value) {
+								ws.current.emit("joinChannel", { channelId: value.id });
+							}
+							if (searchBarState.length) {
+								const search = searchBarState.filter((channel: any) => {
+									if (
+										!userInfo.channels.includes(channel.id) &&
+										value.id !== channel.id
+									) {
+										return channel;
+									}
+								});
+								console.log(search);
+								setSearchBarState(search);
+							}
+						}}
+						renderInput={(params) => {
+							return <TextField {...params} label="Search Channel" />;
+						}}
+					/>
+				</div>
+			</div>
+			<div className={styles.chat}>
+				<div className={styles.chatHistory}>
+					{messages && <Messages myMessages={JSON.parse(messages)}></Messages>}
+				</div>
+				<div className={styles.chatBar}>
+					<TextField
+						variant="outlined"
+						fullWidth
+						label={channelSelected ? "Enter a message ..." : "Select a channel"}
+						onChange={(e: any) => setInputText(e.target.value)}
+						value={inputText}
+						disabled={!channelSelected}
+						onKeyDown={(e) => {
+							if (e.key === "Enter" && inputText) {
+								const channel = databaseChannel.find((value: any) => {
+									return value.id === channelSelected;
+								});
+								const dataToSend = {
+									channelname: channel?.channelname,
+									message: inputText,
+								};
+								ws.current.emit("text", dataToSend);
+								setInputText("");
+							}
+						}}
+					/>
+				</div>
+			</div>
+			<div className={styles.myChats}>
+				<h1>My Chats</h1>
+				<div className={styles.button}>
+					<Button text="Create Channel" onClick={handleOpenModal}>
+						<FontAwesomeIcon icon={faPlusCircle} />
+					</Button>
+					<Modal
+						open={openModal}
+						onClose={handleCloseModal}
+						className={styles.modal}
+					>
+						<Box
+							component="form"
+							sx={{
+								"& > :not(style)": { m: 1, width: "25ch" },
+							}}
+							autoComplete="off"
+							className={styles.boxModal}
+						>
+							<TextField
+								label="Enter a Name ..."
+								variant="standard"
+								onChange={(e: any) => setChannelName(e.target.value)}
+								value={channelName}
+							/>
+							<TextField
+								label="Choose a Password ..."
+								variant="standard"
+								type="password"
+								onChange={(e: any) => setPassword(e.target.value)}
+								value={password}
+							/>
+							<Button
+								text="Create"
+								className={styles.create}
+								onClick={async () => {
+									handleCloseModal();
+									ws.current.emit("createChannel", {
+										channelname: channelName,
+										password: password,
+									});
+								}}
+							/>
+						</Box>
+					</Modal>
+				</div>
+				<div className={styles.trucblanc}>
+					<Channels
+						myChats={channelUserJoined}
+						channelState={setChannelSelected}
+					></Channels>
+				</div>
+			</div>
+		</div>
+	);
 }
