8b6fdd7f496d258f9e3fbfc81c31d5bf4e78c82c
lacruype <lacruype@student.42.com>
2022-05-13 23:23:17 +0200

    THEGAME COMMENCEMENT

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import {
-	Routes,
-	Route,
-	BrowserRouter,
-	Outlet,
-	Navigate,
+  Routes,
+  Route,
+  BrowserRouter,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
 import Chat from "./pages/Chat/Chat";
 import Profile from "./pages/Profile/Profile";
@@ -20,125 +20,126 @@ import { io } from "socket.io-client";
 import TheGame from "./pages/TheGame/TheGame";
 
 function App() {
-	const [ready, setReady] = useState(false);
-	const [context, setContext] = useState<any>();
-	const [userInfo, setUserInfo] = useState<any>();
-	const [ws, setWs] = useState<any>();
+  const [ready, setReady] = useState(false);
+  const [context, setContext] = useState<any>();
+  const [userInfo, setUserInfo] = useState<any>();
+  const [ws, setWs] = useState<any>();
 
-	const ProtectedRoutes = () => {
-		return (
-			<>
-				{Cookies.get("token") !== undefined ? (
-					<Outlet />
-				) : (
-					<Navigate replace to="/Login" />
-				)}
-			</>
-		);
-	};
-	const DefaultRoutes = () => {
-		return (
-			<div>
-				<NavBar />
-				<Routes>
-					<Route path="/" element={<ProtectedRoutes />}>
-						<Route path="/PlayGame" element={<PlayGame />} />
-						<Route path="/TheGame" element={<TheGame />} />
-						<Route path="/Profile" element={<Profile />} />
-						<Route path="/Profile/:id" element={<Profile />} />
-						<Route path="/Leaderboard" element={<Leaderboard />} />
-						<Route path="/Friendlist" element={<Friendlist />} />
-						<Route path="/Chat" element={<Chat />} />
-						<Route path="/Credits" element={<Credits />} />
-						{/* DANS SETTINGS IL Y AURA LOGOUT*/}
-						<Route path="/Settings" element={<Settings />} />
-						<Route path="/" element={<Navigate replace to="/Profile" />} />
-						<Route path="*" element={<div>404</div>} />
-					</Route>
-				</Routes>
-			</div>
-		);
-	};
-	// get the jwt token from the url and put it into a cookie
-	if (Cookies.get("token") === undefined) {
-		const params = new URLSearchParams(window.location.search);
-		const paramValue = params.get("code");
-		if (paramValue !== null) {
-			Cookies.set("token", paramValue, { expires: 1 });
-			window.location.href = window.location.href.split("?")[0];
-		}
-	}
+  const ProtectedRoutes = () => {
+    return (
+      <>
+        {Cookies.get("token") !== undefined ? (
+          <Outlet />
+        ) : (
+          <Navigate replace to="/Login" />
+        )}
+      </>
+    );
+  };
+  const DefaultRoutes = () => {
+    return (
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ProtectedRoutes />}>
+            <Route path="/PlayGame" element={<PlayGame />} />
+            <Route path="/TheGame" element={<TheGame />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Profile/:id" element={<Profile />} />
+            <Route path="/Leaderboard" element={<Leaderboard />} />
+            <Route path="/Friendlist" element={<Friendlist />} />
+            <Route path="/Chat" element={<Chat />} />
+            <Route path="/Credits" element={<Credits />} />
+            {/* DANS SETTINGS IL Y AURA LOGOUT*/}
+            <Route path="/Settings" element={<Settings />} />
+            <Route path="/" element={<Navigate replace to="/Profile" />} />
+            <Route path="*" element={<div>404</div>} />
+          </Route>
+        </Routes>
+      </div>
+    );
+  };
+  // get the jwt token from the url and put it into a cookie
+  if (Cookies.get("token") === undefined) {
+    const params = new URLSearchParams(window.location.search);
+    const paramValue = params.get("code");
+    if (paramValue !== null) {
+      Cookies.set("token", paramValue, { expires: 1 });
+      window.location.href = window.location.href.split("?")[0];
+    }
+  }
 
-	const getUserInfo = async () => {
-		const myData = await fetch(
-			process.env.REACT_APP_BACK_URL +
-				":" +
-				process.env.REACT_APP_BACK_PORT +
-				"/user/me/",
-			{
-				credentials: "include",
-			}
-		);
-		setUserInfo(await myData.json());
-		// setWs(
-		// 	io(
-		// 		process.env.REACT_APP_BACK_URL +
-		// 			":" +
-		// 			process.env.REACT_APP_BACK_PORT +
-		// 			"?ft_id=" +
-		// 			userInfo.ft_id
-		// 	)
-		// );
-		// setContext(createContext(ws));
-	};
+  const getUserInfo = async () => {
+    const myData = await fetch(
+      process.env.REACT_APP_BACK_URL +
+        ":" +
+        process.env.REACT_APP_BACK_PORT +
+        "/user/me/",
+      {
+        credentials: "include",
+      }
+    );
+    setUserInfo(await myData.json());
+    // setWs(
+    // 	io(
+    // 		process.env.REACT_APP_BACK_URL +
+    // 			":" +
+    // 			process.env.REACT_APP_BACK_PORT +
+    // 			"?ft_id=" +
+    // 			userInfo.ft_id
+    // 	)
+    // );
+    // setContext(createContext(ws));
+  };
 
-	useEffect(() => {
-		if (userInfo && userInfo.ft_id) {
-			setWs(
-				io(
-					process.env.REACT_APP_BACK_URL +
-						":" +
-						process.env.REACT_APP_BACK_PORT +
-						"?ft_id=" +
-						userInfo.ft_id
-				)
-			);
-		}
-	}, [userInfo]);
+  useEffect(() => {
+    if (userInfo && userInfo.ft_id) {
+      setWs(
+        io(
+          process.env.REACT_APP_BACK_URL +
+            ":" +
+            process.env.REACT_APP_BACK_PORT +
+            "?ft_id=" +
+            userInfo.ft_id
+        )
+      );
+    }
+  }, [userInfo]);
 
-	useEffect(() => {
-		setContext(createContext(ws));
-	}, [ws]);
+  useEffect(() => {
+    setContext(createContext(ws));
+  }, [ws]);
 
-	useEffect(() => {
-		getUserInfo();
-		setReady(true);
-	}, []);
-	// useEffect(() => {
-	// 	console.log("ready");
-	// 	console.log(userInfo.current);
-	// 	console.log(ws.current);
-	// 	if (userInfo.current && context.current) {
-	// 		setReady(true);
-	// 	}
-	// }, [userInfo.current, context.current, ws.current]);
+  useEffect(() => {
+    getUserInfo();
+    setReady(true);
+  }, []);
+  // useEffect(() => {
+  // 	console.log("ready");
+  // 	console.log(userInfo.current);
+  // 	console.log(ws.current);
+  // 	if (userInfo.current && context.current) {
+  // 		setReady(true);
+  // 	}
+  // }, [userInfo.current, context.current, ws.current]);
 
-	// console.log("userInfo", userInfo);
+  // console.log("userInfo", userInfo);
 
-	return (
-		<>
-			{ready && (
-				<BrowserRouter>
-					<context.Provider value={ws}>
-						<Routes>
-							<Route path="/Login" element={<Login />} />
-							<Route path="*" element={<DefaultRoutes />} />
-						</Routes>
-					</context.Provider>
-				</BrowserRouter>
-			)}
-		</>
-	);
+  return (
+    <>
+      {ready && (
+        <BrowserRouter>
+          <context.Provider value={ws}>
+            <Routes>
+              <Route path="/Login" element={<Login />} />
+              <Route path="/TheGame" element={<TheGame />} />
+              <Route path="*" element={<DefaultRoutes />} />
+            </Routes>
+          </context.Provider>
+        </BrowserRouter>
+      )}
+    </>
+  );
 }
 
 export default App;
--- frontend/src/pages/TheGame/TheGame.tsx
+++ frontend/src/pages/TheGame/TheGame.tsx
@@ -1,5 +1,33 @@
-import styles from "../css/TheGame.module.css";
+import styles from "../../css/TheGame.module.css";
+import * as React from "react";
+import Modal from "@mui/material/Modal";
+import "animate.css";
 
 export default function TheGame() {
-	return <div></div>;
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.machin}>ET VOILA</div>
+      <button onClick={handleOpen}>VICTORY</button>
+      <Modal open={open} onClose={handleClose}>
+        <div className={styles.endscreen}> Victory !</div>
+      </Modal>
+      <button className={`${styles.buttons} ${styles.defeat}`}>DEFEAT</button>
+      <button className={`${styles.buttons} ${styles.goal}`}>GOAL</button>
+      <div className={styles.containerGame}>
+        <div className={styles.score}>
+          <div id="rightScore">0</div>
+          <div id="leftScore">0</div>
+        </div>
+        <div className={`${styles.names} ${styles.leftName}`}>Lacruype</div>
+        <div className={`${styles.names} ${styles.rightName}`}>Rledrin</div>
+        <div className={styles.ball}></div>
+        <div className={`${styles.paddle} ${styles.leftPaddle}`}></div>
+        <div className={`${styles.paddle} ${styles.rightPaddle}`}></div>
+      </div>
+    </div>
+  );
 }
