090d13bb0a253c0685eb2debeb12ab9d2b0be5c0
lacruype <lacruype@student.42.com>
2022-05-14 17:28:51 +0200

    thegame clignote

--- frontend/src/pages/TheGame/TheGame.tsx
+++ frontend/src/pages/TheGame/TheGame.tsx
@@ -2,11 +2,16 @@ import styles from "../../css/TheGame.module.css";
 import * as React from "react";
 import Modal from "@mui/material/Modal";
 import "animate.css";
+import { wait } from "@testing-library/user-event/dist/utils";
+import { getElementError } from "@testing-library/react";
 
 export default function TheGame() {
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [open1, setOpen1] = React.useState(false);
+  const handleOpen1 = () => setOpen1(true);
+  const handleClose1 = () => setOpen1(false);
 
   return (
     <div className={styles.page}>
@@ -17,11 +22,38 @@ export default function TheGame() {
         VICTORY
       </button>
       <Modal open={open} onClose={handleClose}>
-        <div className={styles.endscreen}> Victory !</div>
+        <div
+          className={`${styles.endscreen} animate__animated animate__zoomInUp`}
+          style={{ color: "gold" }}
+        >
+          Victory !
+        </div>
       </Modal>
-      <button className={`${styles.buttons} ${styles.defeat}`}>DEFEAT</button>
-      <button className={`${styles.buttons} ${styles.goal}`}>GOAL</button>
-      <div className={styles.containerGame}>
+      <button
+        className={`${styles.buttons} ${styles.defeat}`}
+        onClick={handleOpen1}
+      >
+        DEFEAT
+      </button>
+      <Modal open={open1} onClose={handleClose1}>
+        <div
+          className={`${styles.endscreen} animate__animated animate__hinge`}
+          style={{ color: "red" }}
+        >
+          Defeat ...
+        </div>
+      </Modal>
+      <button
+        className={`${styles.buttons} ${styles.goal}`}
+        onClick={() => {
+          document
+            .getElementById("containerGame")
+            ?.classList.toggle(styles.clignoter);
+        }}
+      >
+        GOAL
+      </button>
+      <div className={styles.containerGame} id="containerGame">
         <div className={styles.score}>
           <div id="rightScore">0</div>
           <div id="leftScore">0</div>
